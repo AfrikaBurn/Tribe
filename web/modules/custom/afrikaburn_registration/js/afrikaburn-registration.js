0e595c445184cc2cf8b8d114dd40ee61b2de0802
Ubuntu <ubuntu@ip-172-31-0-102.us-west-2.compute.internal>
2018-05-30 13:59:21 +0000

    Reg error jumping works

--- web/modules/custom/afrikaburn_registration/js/afrikaburn-registration.js
+++ web/modules/custom/afrikaburn_registration/js/afrikaburn-registration.js
@@ -17,26 +17,25 @@
         }
       );
 
-      $('form.user-form', context).find('#edit-submit').click(
-        function(){
+      var
+	root = $('.user-form', context),
+        panels = root.find('.details-wrapper'),
+        tabs = root.find('.vertical-tabs__menu, .horizontal-tabs-list').children()
 
-          var
-            root = $(this).parents('form').first(),
-            panels = root.find('.details-wrapper'),
-            tabs = root.find('.vertical-tabs__menu, .horizontal-tabs-list')
+      function jumpToError(){
+        setTimeout(
+          function(){
+            var
+              firstErrorPanel = root.find('.error:visible').first().parents('.details-wrapper'),
+              firstErrorTab = tabs[panels.index(firstErrorPanel)]
+            $(firstErrorTab).find('a').click()
+          },
+          100
+        )
+      }
 
-          setTimeout(
-            function(){
-              var
-                firstErrorPanel = root.find('.error:visible').first().parents('.details-wrapper'),
-                firstErrorTab = tabs[panels.index(firstErrorPanel)]
-
-              $(firstErrorTab).find('a').click()
-            },
-            100
-          )
-        }
-      )
+      root.find('#edit-submit').click(jumpToError)      
+      jumpToError()
 
       // Text size fixing
       $('.user-form .form-text, .user-form .form-email').removeAttr('size')
