7a1150afd329dec3e609860ca7fbeee09517cfb9
Scheepers <scheepers.debruin@gmail.com>
2019-01-29 16:07:34 +0200

    WAPs

--- web/modules/custom/afrikaburn_registration/js/afrikaburn-project-registration.js
+++ web/modules/custom/afrikaburn_registration/js/afrikaburn-project-registration.js
@@ -10,6 +10,30 @@
 
   Drupal.behaviors.afrikaburnRegistration = {
     attach: function (context, settings) {
+
+      function checkDups(){
+        $('#wap-assign .form-select option')
+          .removeAttr('disabled')
+
+        $('#wap-assign .form-select option:selected:not([value="0"])')
+          .each(
+            (index, element) => {
+              var
+                selector = '#wap-assign .form-select option[value="' + $(element).attr('value') + '"]',
+                option = $(selector),
+                other = option.not(element)
+
+              other.attr('disabled', 'disabled')
+            }
+          )
+      }
+      checkDups()
+
+      $(
+        () => {
+          $('#wap-assign .form-select', context).change(checkDups)
+        }
+      )
     }
   }
 
